8b307e0338901644ad73f85b024606fac2eb04e5
Elena Lee <jelee.elena@gmail.com>
2020-05-25 14:17:28 +0900

    Fix app.js route item

--- routes/item.js
+++ routes/item.js
@@ -1,10 +1,6 @@
 const router = require("express").Router();
 const { itemcontroller } = require("../controller");
 
-// router.get("/", (req, res) => {
-//   res.status(200).send("");
-// });
-
 // * POST /item
 router.post("/", itemcontroller.addItem.post);
 
