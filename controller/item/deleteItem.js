5153311bb5380bc4f7d030de88112479b0b6f4a2
Elena Lee <jelee.elena@gmail.com>
2020-05-26 02:14:29 +0900

    Fix delete with no image_id

--- controller/item/deleteItem.js
+++ controller/item/deleteItem.js
@@ -3,10 +3,9 @@ const { items, images } = require("../../models");
 module.exports = {
   delete: (req, res) => {
     const item_id = req.query.item_id;
-    const { user_id, image_id } = req.body;
+    const { user_id } = req.body;
 
     let item = items.findOne({ where: { id: item_id, user_id: user_id } });
-    let image = images.findOne({ where: { id: image_id } });
 
     if (!item) {
       res.status(404).send("Cannot find the item");
@@ -14,8 +13,8 @@ module.exports = {
       items
         .destroy({ where: { user_id: user_id, id: item_id } })
         .then(() => {
-          if (image) {
-            images.destroy({ where: { id: image_id } });
+          if (req.body.image_id) {
+            images.destroy({ where: { id: req.body.image_id } });
           }
           res.status(202).send("Item has been successfully deleted");
         })
