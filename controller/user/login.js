d472c8e34ead31de55b20fee9360d656983675fb
NanMinYeong <nmy0502@gmail.com>
2020-05-26 15:32:56 +0900

    Add session

--- controller/user/login.js
+++ controller/user/login.js
@@ -13,15 +13,17 @@ module.exports = {
           const decodePW = decrypt(result);       
           // ? 복호화된 비밀번호와 req로 받은 비밀번호 비교
           if(decodePW === password) {
-            // ! 토큰 생성 req.headers.cookie에 생성
             const user_info = addToken(result);
-            //console.log("---incookie", user_info.token)
-
-            // ! 토큰 암호화
             const token = enToken(user_info);
-            //console.log("---enToken", token)
+            
+            req.session.id = result.dataValues.id;
+            // console.log("---user_id",result.dataValues.id);
+            // console.log("---session_id",req.session.id);
+            // console.log(res.req.session);
 
-            res.status(200).cookie('token', user_info.token)
+            res.status(200)
+              .cookie('session_id', req.session.id)
+              .cookie('token', user_info.token)
               .json({
                 user_id: result.dataValues.id,
                 user_name: result.dataValues.user_name,
--- controller/user/logout.js
+++ controller/user/logout.js
@@ -1,9 +1,22 @@
 module.exports = {
   post: (req, res) => {
-    if(req.cookies.token) {
-      res.clearCookie("token", {path: '/'});
+    if(req.session.id) {
+      //console.log(" ---LOGOUT--- req.session",req.session)
+      req.session.destroy(err => {
+        if(err) {
+          console.log(err);
+        } else {
+          //console.log(" ---DESTORY--- req.session",req.session)
+          res.clearCookie('session_id')
+          res.clearCookie('token', {path: '/'});
+          res.redirect('/');
+          res.end();
+        }
+      });
+    } else {
+      console.log('NO SESSION ID');
+      res.redirect('/');
+      res.end();
     }
-    res.redirect('/');
-    res.end();
   }
 };
