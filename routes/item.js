6f326453dc861246c4f24ddf171c56369ba3f068
Elena Lee <jelee.elena@gmail.com>
2020-05-26 16:32:11 +0900

    Add editItem api

--- routes/item.js
+++ routes/item.js
@@ -7,6 +7,9 @@ router.post("/", itemcontroller.addItem.post);
 // * DELETE /item?item-id=${item_id}
 router.delete("/:item_id?", itemcontroller.deleteItem.delete);
 
+// * PATCH /item?item-id=${item_id}
+router.patch("/:item_id?", itemcontroller.editItem.patch);
+
 router.get("/:item_id?", (req, res) => {
   res.status(200).send("get success");
 });
