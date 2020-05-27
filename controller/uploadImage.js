fa5efc8f0a7abbdcfdb833c1526d517a77922b16
Elena Lee <jelee.elena@gmail.com>
2020-05-27 09:01:34 +0900

    Add uploadImage api

---
+++ controller/uploadImage.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const AWS = require("aws-sdk");
+const { images } = require("../models");
+// const { accessKeyId, secretAccessKey } = require("../config/config.json");
+
+// ! SDK 로딩, (인증 자격 증명 로드)
+AWS.config.loadFromPath("config/s3config.json");
+
+module.exports = {
+  uploadImage: (req, res) => {
+    // ! S3 객체 생성
+    let s3 = new AWS.S3();
+
+    let upload = multer({
+      storage: multerS3({
+        s3: s3,
+        bucket: "elenajelbucket",
+        key: function(req, file, cb) {
+          let extension = path.extname(file.originalname);
+          cb(null, `image/${Date.now().toString()}${extension}`);
+        },
+        acl: "public-read",
+      }),
+    });
+
+    let uploadSingle = upload.single("image_file");
+
+    uploadSingle(req, res, (err) => {
+      if (req.file === undefined || req.file === null) {
+        res.end(err.message);
+      } else if (req.file) {
+        images.create({
+          image_file: req.file.location,
+        });
+        res.status(200).json({
+          image_file: req.file.location,
+        });
+      } else {
+        res.status(500).send("Sever error");
+      }
+    });
+  },
+};
